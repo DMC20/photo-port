9312be1ff0251b1ed4e570f39ea758577a4a70d3
Daniel Carazo <danielcarazo20@gmail.com>
2021-09-30 18:38:59 -0400

    test

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -25,25 +25,26 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setCurrentCategory(false)}>
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
           <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <span onClick={() => handleClick('Contact')}>
-              Contact
-            </span>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
                 currentCategory.name === category.name && !contactSelected && 'navActive'
                 }`}
-              key={category.name}>
-              <span onClick={() => {
+              key={category.name}
+            >
+              <span
+                onClick={() => {
                   setCurrentCategory(category);
-                  setContactSelected(false)
-                }}>
+                  setContactSelected(false);
+                }}
+              >
                 {capitalizeFirstLetter(category.name)}
               </span>
             </li>
